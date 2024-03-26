ad711ef2d77ddd1b7a7a979323cf0e3673d28d1c
KarolWes <karol.wesolowski01@gmail.com>
2024-03-26 15:52:50 +0100

    ex 5-6 done

--- src/com/example/bigdata/EsperClient.java
+++ src/com/example/bigdata/EsperClient.java
@@ -30,11 +30,13 @@ public class EsperClient {
         EPDeployment deployment = compileAndDeploy(runtime, """
               @public @buseventtype create json schema JokeEvent(character string, quote string, people_in_room int, laughing_people int, pub string, ets string, its string);
                
-              @name('answer') select pub, its.substring(11, 16) as its_start,
-              from JokeEvent#time_batch(1 min)
-              group by its.substring(11, 16), pub
-              having sum (case when laughing_people * 2 > people_in_room then 1 else (case when laughing_people * 2 = people_in_room then 0 else -1 end) end) < 0;
-                 """);
+               
+              create window McL#length(100) as JokeEvent;
+              insert into McL select * from JokeEvent;
+              
+              @name('answer') select m[0].laughing_people as lp1, m[1].laughing_people as lp2, m[2].laughing_people as lp3
+              from pattern[ every ([3] m=JokeEvent(laughing_people >= 4) until JokeEvent(people_in_room >= 30))] ;
+              """);
 
         SimpleListener listener = new SimpleListener();
         // Add a listener to the statement to handle incoming events
@@ -42,8 +44,11 @@ public class EsperClient {
             statement.addListener(listener);
         }
 
-        InputStreamGenerator generator = new InputStreamGenerator(noOfRecordsPerSec, howLongInSec);
-        generator.generate(runtime, true);
+//        InputStreamGenerator generator = new InputStreamGenerator(noOfRecordsPerSec, howLongInSec);
+//        generator.generate(runtime, true);
+        for (String s : createInputData()) {
+            runtime.getEventService().sendEventJson(s, "JokeEvent");
+        }
 
 
 
@@ -82,7 +87,41 @@ public class EsperClient {
         }
         return epCompiled;
     }
-
+    static String[] createInputData() {
+        return new String[] {
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":0, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:00:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:01:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":3, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:02:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":3, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:03:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":10, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:04:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":5, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:05:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":5, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:06:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:07:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:08:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":4, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:09:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:10:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":7, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:11:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"Bob's\", \"ets\":0, \"its\":\"2011-04-01 00:12:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":0, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:13:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":5, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:14:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":3, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:15:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":4, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:16:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":32, \"laughing_people\":4, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:17:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":5, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:18:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":8, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:19:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":7, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:20:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":8, \"pub\":\"Bob's\", \"ets\":0, \"its\":\"2011-04-01 00:21:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:22:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":7, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:23:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:24:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":0, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:25:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":0, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:26:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":0, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:27:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":4, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:28:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:29:00.0\"}",
+                "{\"character\":\"\", \"quote\":\"\", \"people_in_room\":0, \"laughing_people\":1, \"pub\":\"McLaren's\", \"ets\":0, \"its\":\"2011-04-01 00:30:00.0\"}"
+        };
+    }
 
 }
 
